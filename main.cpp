9527a477771bccc7d519c230fac7bda4178295f6
Thomas Rubeli <thomas.rubeli@epfl.ch>
2013-04-05 19:30:11 +0200

    test

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 5 avr. 2013
+ *      Author: Aech
+ */
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+	setvbuf(stdout, NULL, _IONBF, 0);
+	setvbuf(stderr, NULL, _IONBF, 0);
+	printf("lol kebab");
+}
+
