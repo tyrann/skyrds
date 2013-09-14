5254fa2f35455d92ec49146b719bcfffe0696cef
Thomas Rubeli <thomas.rubeli@epfl.ch>
2013-09-14 15:35:02 +0200

    suspension system OK

--- Assets/SpaceshipBehaviour.cs
+++ Assets/SpaceshipBehaviour.cs
@@ -9,15 +9,28 @@ public class SpaceshipBehaviour : MonoBehaviour
 	public GameObject spaceshipPrefab;
 	public float force;
 	public float gravity = Physics.gravity.y;
+	private BoxCollider boxCollider;
+	private float stability = 1;
+	private Vector3[] cornersPoint = new Vector3[5];
+	private  int nbSuspensions= 5;
+	private Transform[] corners = new Transform[5];
+	private Vector3 boxDimension;
+	private bool physicsSet = false;
+	
+	
 	// Use this for initialization
 	void Start ()
 	{
-		init_physics();
+		init_physics ();		
 	}
 	
 	void Respawn ()
 	{
 		Instantiate (spaceshipPrefab, GameObject.FindGameObjectWithTag ("Respawn").transform.position, Quaternion.identity);
+		for (int i =0; i<=nbSuspensions-1; i++) {	
+			//DESTROY THE CHILD SUSPENSION
+
+		}
 		Destroy (gameObject);
 	}
 	
@@ -28,7 +41,7 @@ public class SpaceshipBehaviour : MonoBehaviour
 		float forwardMovement = (Input.GetAxis ("Vertical") > 0) ? Input.GetAxis ("Vertical") * speed * Time.deltaTime : 0f;
 		float horizontalMovement = Input.GetAxis ("Horizontal") * speed * Time.deltaTime;
 		
-//		transform.Translate (Vector3.forward * forwardMovement + Vector3.right * horizontalMovement);
+		transform.Translate (Vector3.forward * forwardMovement + Vector3.right * horizontalMovement);
 //				
 //		if (Input.GetButtonDown ("Jump") && !isFalling) {
 //			transform.rigidbody.AddForce (new Vector3 (0, 200, 0), ForceMode.Acceleration);
@@ -42,7 +55,44 @@ public class SpaceshipBehaviour : MonoBehaviour
 //			transform.rigidbody.AddForce(new Vector3(0, (-gravity * rigidbody.mass)*(1/distanceFromGround), 0), ForceMode.Force);
 //		}
 	}
-	void init_physics(){
+
+	void init_physics ()
+	{
+
+
+//Creates a box around the hovering object
+
+		
+		boxCollider = spaceshipPrefab.GetComponent ("BoxCollider") as BoxCollider;
+
+		boxDimension = new Vector3 (boxCollider.size.x * spaceshipPrefab.transform.localScale.x, boxCollider.size.y * spaceshipPrefab.transform.localScale.y, boxCollider.size.z * spaceshipPrefab.transform.localScale.z) * stability;
+		cornersPoint [0] = new Vector3 (transform.position.x - boxDimension.x / 2, transform.position.y - boxDimension.y / 2, transform.position.z + boxDimension.z / 2);
+		cornersPoint [1] = new Vector3 (boxDimension.x / 2 + transform.position.x, transform.position.y - boxDimension.y / 2, transform.position.z + boxDimension.z / 2);
+		cornersPoint [2] = new Vector3 (boxDimension.x / 2 + transform.position.x, transform.position.y - boxDimension.y / 2, transform.position.z - boxDimension.z / 2);
+		cornersPoint [3] = new Vector3 (transform.position.x - boxDimension.x / 2, transform.position.y - boxDimension.y / 2, transform.position.z - boxDimension.z / 2);
+		cornersPoint [4] = transform.position;
+
+
+		for (int i =0; i<=cornersPoint.Length-1; i++) {
+
+			GameObject suspension = GameObject.CreatePrimitive (PrimitiveType.Sphere);
+
+			suspension.name = "Suspension" + "(" + i + ")";
+
+			suspension.transform.parent = spaceshipPrefab.transform;
+
+			suspension.transform.localPosition = transform.InverseTransformPoint (cornersPoint [i]);
+
+			corners [i] = suspension.transform;
+
+			Destroy (suspension.GetComponent ("MeshRenderer"));
+			Destroy (suspension.GetComponent ("Collider"));
+
+		}
+
+		cornersPoint = null;
+
+		physicsSet = true;
 		
 	}
 	
