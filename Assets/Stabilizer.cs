a879489406b292d755f1d428b88ae9a7e51356f7
thomasrubeli <thomas.rubeli@epfl.ch>
2013-09-16 06:06:32 +0200

    ship stabilizer + map extension

---
+++ Assets/Stabilizer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stabilizer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		Quaternion rotation = Quaternion.Slerp(transform.localRotation,Quaternion.Euler(new Vector3(0,0,0)),Time.deltaTime * 1);
+
+		transform.localRotation = rotation;
+	
+	}
+}
